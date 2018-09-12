55101895ff8a9e496704800b00811da4ce037964
caiofb47 <caiofb47@Tizil>
2018-09-12 00:54:36 -0300

    Agora s

--- compilador/Lexer.py
+++ compilador/Lexer.py
@@ -7,20 +7,23 @@ from Tag import Tag
 
 
 # Variaveis GLOBAIS
-END_OF_FILE = 0 # TEM Q SER |Constante| para fim do arquivo
-lookahead = 0 # Armazena o ultimo caractere lido no arquivo
+END_OF_FILE = 0 # OK     TEM Q SER |Constante| para fim do arquivo
 n_line = 1 # Contador de linhas
 n_column = 1 # Contador de colunas
 instance_file = '' # Referencia para o arquivo
 
+
 class Lexer:
 	
-	# Lendo arquivo
-	@staticmethod # https://stackoverflow.com/questions/4473184/unbound-method-f-must-be-called-with-fibo-instance-as-first-argument-got-cla
-	def abrirArq(self, input_data):
+	#@staticmethod # https://stackoverflow.com/questions/4473184/unbound-method-f-must-be-called-with-fibo-instance-as-first-argument-got-cla
+	def Lexer(self, input_data):
 		try:
-			instance_file = open(input_data,'r')
+			
+			# Lendo arquivo
+			lido = open(input_data,'r')
+			instance_file = lido.read()
 		
+			END_OF_FILE = len(input_data) - 1 # Definindo o tamanho do arquivo
 		# Excecao de "No such file or directory"
 		except IOError as e:
 			print('Erro de abertura do arquivo', e)
@@ -30,10 +33,11 @@ class Lexer:
 	def proxToken(self):
 		
 		# Variaveis do AUTOMATO FD
+		lookahead = 0 # Armazena o ultimo caractere lido no arquivo	
 		lexema = ''
 		estado = 1
 		c = ''
-		END_OF_FILE = instance_file.len() - 1 # Definindo o tamanho do arquivo
+		
 		
 		while(True):
 			
@@ -51,7 +55,8 @@ class Lexer:
 			# Movimentacao do AFD
 			if (estado == 1):
 				if(lookahead == END_OF_FILE):
-					return Token(Tag['END_OF_FILE'], 'EOF', n_line, n_column)
+					# TA EM LOOP
+					return Token().Token(Tag['END_OF_FILE'], 'EOF', n_line, n_column)
 				
 				
 			
@@ -64,9 +69,11 @@ class Lexer:
 # Main :D
 if __name__ == '__main__':
 	
-	# Variaveis # /home/caiofb47/text
+	# Variaveis 
+	# /home/caiofb47/text
 	token = Token()
-	lexer = Lexer.abrirArq(self, input_data)
+	lexer = Lexer()
+	lexer.Lexer('/home/caiofb47/text')
 	
 	
 		
