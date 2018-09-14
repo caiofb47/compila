4bcb1e50d8795b269747daa3af975230b52a6bf1
caiofb47 <caiofb47@Tizil>
2018-09-14 16:30:13 -0300

    Terminando logica basica e comecando movimentacao :D

--- compilador/Lexer.py
+++ compilador/Lexer.py
@@ -1,89 +1,104 @@
+# Imports
 from _ast import TryExcept
 from Token import Token
 from Tag import Tag
 
 
+
+
 # Procurar pela tag "Rever"
 
-
-# Variaveis GLOBAIS
-END_OF_FILE = 0 # OK     TEM Q SER |Constante| para fim do arquivo
-n_line = 1 # Contador de linhas
-n_column = 1 # Contador de colunas
-instance_file = '' # Referencia para o arquivo
-
+# Caio Fabio Gomes Alves - caiofb47@gmail.com
 
 class Lexer:
 	
-	#@staticmethod # https://stackoverflow.com/questions/4473184/unbound-method-f-must-be-called-with-fibo-instance-as-first-argument-got-cla
+	
+	# Construtor/Inicializador da classe LEXER
+	def __init__(self):
+		self.instance_file = '' # Referencia para o arquivo
+		self.END_OF_FILE = 0 #TEM Q SER |Constante| para fim do arquivo
+		self.n_line = 0
+		self.n_column = 0
+		
+		
+	# Funcao para ler o arquivo
 	def Lexer(self, input_data):
+		# Tentar abrir e ler arquivo
 		try:
 			
-			# Lendo arquivo
+			# Abrindo o arquivo
 			lido = open(input_data,'r')
-			instance_file = lido.read()
+			# Lendo o arquivo
+			self.instance_file = lido.read()
+			
+			# Pegando o tamanho do arquivo com a funcao len
+			self.END_OF_FILE = len(self.instance_file) - 1
 		
-			END_OF_FILE = len(input_data) - 1 # Definindo o tamanho do arquivo
 		# Excecao de "No such file or directory"
 		except IOError as e:
-			print('Erro de abertura do arquivo', e)
+			print('Erro de abertura do arquivo:', e)
 			exit(1)
 		
-	# Metodo que simula o AUTOMATO FD
+	# Metodo que simula o AUTOMATO Finito Determinisitico
 	def proxToken(self):
 		
 		# Variaveis do AUTOMATO FD
-		lookahead = 0 # Armazena o ultimo caractere lido no arquivo	
+		lookahead = 0 # Armazena a posicao do ultimo caractere lido no arquivo	
 		lexema = ''
 		estado = 1
 		c = ''
-		
+
 		
 		while(True):
-			
+		
 			# Avanca caractere ou retorna um token
 			try:
 				# Como python le direto o arquivo, n precisa converter
-				if (lookahead != END_OF_FILE):
-					c = instance_file[lookahead] # Le um caractere
+				if (lookahead != self.END_OF_FILE):
+					c = self.instance_file[lookahead] # Le um caractere
 					lookahead += 1 # Muda o apontamento
-			# Rever essa exception
+					
+					
+			# Rever essa exception, pq n e de entrada e saida :/
 			except IOError as e:
 				print("Erro na leitura do caractere")
 				
 			
+			
 			# Movimentacao do AFD
 			if (estado == 1):
-				if(lookahead == END_OF_FILE):
-					# TA EM LOOP
-					return Token().Token(Tag['END_OF_FILE'], 'EOF', n_line, n_column)
 				
-				
+				if(lookahead == self.END_OF_FILE):
+					# Retorna um novo token
+					return Token().Token(Tag['END_OF_FILE'], 'EOF', self.n_line, self.n_column)
+				elif(c == ' ' or c == '\t' or c =='\n' or c =='\r'):
+					# Permanece no estado 1
+					if(c == '\n' or c == '\r'):
+						self.n_line += 1 # Mudanca de linha
 			
 					
-				
-				
-				
-
-
+			
 # Main :D
 if __name__ == '__main__':
 	
 	# Variaveis 
-	# /home/caiofb47/text
 	token = Token()
 	lexer = Lexer()
 	lexer.Lexer('/home/caiofb47/text')
 	
-	
-		
-	# Enquanto nao houver erros ou nao for o fim do arquivo:
+	# Enquanto nao houver erros
 	while(True):
 		token = lexer.proxToken()
-		
 		# Imprimindo TOKEN
 		if(token != None):
-			print('Token: ', token.__srt__, '\t Linha: ', n_line, '\t Coluna', n_column)
+			print('Token: ', token.__srt__)
+		# Test se a funcao .proxToken() retorna
+		#else:
+			#print('N retorna')
+			
+		# Break caso fim de arquivo
+		if(token != None and token.nomeGet() != Tag['END_OF_FILE']):
+			break
 	
 	
 
